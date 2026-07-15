078f347dc043ce908e722c34b6de8b91d7aeeaa3
thekingprediction-maker <thekingprediction@gmail.com>
2026-07-15 14:00:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -302,7 +302,7 @@ html_code = """
 </div>
 
 <script>
-const API_KEY = "aa5e53f893088010cc7c47af17f306e9";
+const API_KEY = "3e90e10f6eefd6349e825d3499bcbe8d";
 const BASE_CSV_URL = "https://raw.githubusercontent.com/thekingprediction-maker/DATABASE_AVANZATO_2025.csv/main/";
 const REFS_FILE = "ARBITRI_SERIE_A%20-%20Foglio1.csv";
 let currentLeague = 7286, dbXG = [];
